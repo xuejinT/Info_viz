87b1f762dc39234a2c5393c34143c0f6696aab0f
xuejinT <xuejint02@gmail.com>
2019-11-19 13:08:18 -0500

    added dot plot
    - added basic dot plot based on views

--- overview.js
+++ overview.js
@@ -2,24 +2,28 @@ var svg = d3.select('svg');
 
 // Original lab3 codes
 // Create x-scale for positioning the circles
-var xScale = d3.scaleLinear()
+var radius = 3;
+var xScale = d3.scaleTime()
    .range([100,1000]);
 
 // Create y-scale for positioning the circles
-var yScale = d3.scaleLog()
-   .range([50,550]);
+var yScale = d3.scaleLinear()
+   .range([550,50]);
 
-// Create square root scale to scale the Area of the circles
-// Note that area is a more effective visual encoding
-// var rScale = d3.scaleSqrt()
-//    .range([0,20]);
+var parseTime = d3.timeParse("%Y-%m-%d %H:%M:%S");
 
 // Create a quantize scale for the three habital zone bins
 var colorScale = d3.scaleQuantize()
    .range(['#d64d3f', '#96ac3d', '#208d8d']);
+var tooltip = d3.select(".tooltip");
 
 // Load the dataset
-d3.dsv('\\','US_final.csv').then(function(dataset) {
+d3.dsv('\\','./data/US_final.csv').then(function(dataset) {
+	dataset.forEach(function(d) {
+      d.publish_time = parseTime(d.publish_time);
+      d.trending_date = parseTime(d.trending_date);
+  });
+
 	videos = dataset;
 	console.log(videos);
 
@@ -36,27 +40,45 @@ d3.dsv('\\','US_final.csv').then(function(dataset) {
    xScale.domain(dateExtent);
 
    // Create a d3 selection for the videos
-   // svg.selectAll('.planet')
-   //     .data(videos) // Data-bind the videos array to the d3-selection
-   //     .enter() // Enter - selects incoming data-bound elements
-   //     .append('circle') // Append - append a circle for each planet
-   //     .attr('class', 'planet') // Add the classname that we selected on
-   //     .attr('r', function(d){
-   //         // Set the radius attribute based on the planet's radius
-   //         return rScale(d['radius']);
-   //     })
-   //     .attr('cx', function(d){
-   //         // Set the x-position based on the hzd value
-   //         return xScale(d['habital_zone_distance']);
-   //     })
-   //     .attr('cy', function(d){
-   //         // Set the y-position based on the mass value
-   //         return yScale(d['mass']);
-   //     })
-   //     .style('fill', function(d){
-   //         // Set the fill color based on the hzd value
-   //         return colorScale(d['habital_zone_distance']);
-   //     });
+   svg.selectAll('.video')
+       .data(videos) // Data-bind the videos array to the d3-selection
+       .enter() // Enter - selects incoming data-bound elements
+       .append('circle')
+       .attr('class', 'video') // Add the classname that we selected on
+       .attr('r', function(d){
+           // Set the radius attribute based on the planet's radius
+           return radius;
+       })
+       .attr('cx', function(d){
+           // Set the x-position based on the hzd value
+           return xScale(d.trending_date);
+       })
+       .attr('cy', function(d){
+           // Set the y-position based on the mass value
+           return yScale(d.views);
+       })
+       .style('fill', function(d){
+           // Set the fill color based on the hzd value
+           return colorScale(d.views);
+       })
+       .style('opacity', function(d){
+       	return 0.5;
+       })
+       .on("mouseover", function(d) {		
+            tooltip.transition()		
+                .duration(200)		
+                .style("opacity", .9);		
+            tooltip.html(d["title"] + "<br/>"  + d["views"])
+                .style("left", (d3.event.pageX) + "px")		
+                .style("top", (d3.event.pageY - 28) + "px")
+                .style("cursor","pointer");
+
+            })					
+        .on("mouseout", function(d) {		
+            tooltip.transition()		
+                .duration(500)		
+                .style("opacity", 0);	
+        });
 
    // Create axes for both the y- and x-scale
    svg.append('g') // Append a g element for the scale
@@ -76,7 +98,7 @@ d3.dsv('\\','US_final.csv').then(function(dataset) {
 
    svg.append('g')
        .attr('class', 'x axis')
-       .attr('transform', 'translate(530,0)')
+       .attr('transform', 'translate(1100,0)')
        .call(d3.axisRight(yScale));
 
    // Challenge Problems
@@ -105,30 +127,30 @@ d3.dsv('\\','US_final.csv').then(function(dataset) {
    //     });
 
    // top x axis label
-   svg.append("text")
-       .attr("class","axisLabel")
-       .attr("transform","translate(300,10)")
-       .attr("dy","0.3em")
-       .text("Habital Zone Distance");
+	   // svg.append("text")
+	   //     .attr("class","axisLabel")
+	   //     .attr("transform","translate(300,20)")
+	   //     .attr("dy","0.3em")
+	   //     .text("Date");
 
-   // bottom x axis label
-   svg.append("text")
-       .attr("class","axisLabel")
-       .attr("transform","translate(300,720)")
-       .attr("dy","0.3em")
-       .text("Habital Zone Distance");
+	   // // bottom x axis label
+	   // svg.append("text")
+	   //     .attr("class","axisLabel")
+	   //     .attr("transform","translate(300,560)")
+	   //     .attr("dy","0.3em")
+	   //     .text("Date");
 
    // left y axis label
    svg.append("text")
        .attr("class","axisLabel")
        .attr("transform","translate(20,370)rotate(270)")
        .attr("dy","0.3em")
-       .text("Planet Mass (relative to Earth)");
+       .text("Views");
 
    // right y axis label
    svg.append("text")
        .attr("class","axisLabel")
-       .attr("transform","translate(580,370)rotate(270)")
+       .attr("transform","translate(1150,370)rotate(270)")
        .attr("dy","0.3em")
-       .text("Planet Mass (relative to Earth)");
+       .text("Views");
 });
