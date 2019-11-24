a02d720800b9137f9eb9ef27055fcdd0427b9152
xuejinT <xuejint02@gmail.com>
2019-11-24 17:40:31 -0500

    added likes, dislikes, temperature plot

--- overview.js
+++ overview.js
@@ -1,8 +1,20 @@
+var curr_y = "views";
+
 var d_width = $(".scatterplot").width();
 var d_height = $(".scatterplot").height();
 
-var margin = {top: 20, right: 20, bottom: 110, left: 200},
-    margin2 = {top: 20, right: 1060, bottom: 110, left: 100},
+var margin = {
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 120
+    },
+    margin2 = {
+        top: 20,
+        right: 1060,
+        bottom: 20,
+        left: 50
+    },
     width = d_width - margin.left - margin.right,
     width2 = d_width - margin2.left - margin2.right,
     height = d_height - margin.top - margin.bottom,
@@ -12,16 +24,21 @@ var parseTime = d3.timeParse("%Y-%m-%d %H:%M:%S");
 
 var x = d3.scaleTime().range([0, width]),
     x2 = d3.scaleTime().range([0, width2]),
-    y2 = d3.scaleLinear().range([height,0]),
-    y = d3.scaleLinear().range([height,0]);
+    y2 = d3.scaleLinear().range([height, 0]),
+    y = d3.scaleLinear().range([height, 0]);
 
 var xAxis = d3.axisBottom(x),
     yAxis2 = d3.axisLeft(y2),
     yAxis = d3.axisLeft(y);
 
+
+
 var brush_flag = 0;
 var brush = d3.brushY()
-    .extent([[0, 0],[width2, height]]) 
+    .extent([
+        [0, 0],
+        [width2, height]
+    ])
     .on("brush", brushed);
 
 // var svg = d3.select("body").append("svg")
@@ -46,37 +63,62 @@ var context = svg.append("g")
     .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
 
 d3.dsv('\\', './data/US_final.csv').then(function(data) {
-  // if (error) throw error;
+    // if (error) throw error;
     data.forEach(function(d) {
         d.publish_time = parseTime(d.publish_time);
         d.trending_date = parseTime(d.trending_date);
     });
+    videos = data;
 
-    var viewExtent = d3.extent(data, function(d) {
+    quadtree = d3.quadtree()
+        .extent([
+            [0, 0],
+            [width, height]
+        ])
+        .x(function(d) {
+            return d.trending_date
+        })
+        .y(function(d) {
+            return d.views
+        })
+        .addAll(data);
+
+    quadData = quadtree.data();
+
+
+    var yExtent = d3.extent(data, function(d) {
         return +d['views'];
     });
-    
+
+
     var dateExtent = d3.extent(data, function(d) {
         return +d['trending_date'];
     });
+    var yOffset = 0.01 * (yExtent[1] - yExtent[0]);
+    var xOffset = 0.01 * (dateExtent[1] - dateExtent[0]);
+    yExtent = [yExtent[0] - yOffset, yExtent[1] + yOffset];
+    dateExtent = [dateExtent[0] - xOffset, dateExtent[1] + xOffset];
 
-    // console.log(dateExtent);
     x.domain(dateExtent);
-    y.domain(viewExtent);
-  x2.domain(x.domain());
-  y2.domain(y.domain());
+    y.domain(yExtent);
+    x2.domain(x.domain());
+    y2.domain(y.domain());
 
-// append scatter plot to main chart area 
- var dots = focus.append("g");
+    // append scatter plot to main chart area 
+    var dots = focus.append("g");
     dots.attr("clip-path", "url(#clip)");
     dots.selectAll("dot")
         .data(data)
         .enter().append("circle")
         .attr('class', 'dotFocus')
-        .attr('r',radius)
-        .attr("cx", function(d) { return x(d.trending_date); })
-        .attr("cy", function(d) { return y(d.views); })
-        .style('fill', color) 
+        .attr('r', radius)
+        .attr("cx", function(d) {
+            return x(d.trending_date);
+        })
+        .attr("cy", function(d) {
+            return y(d.views);
+        })
+        .style('fill', color)
         .style('opacity', 0.5)
         .on("mouseover", function(d) {
             tooltip.transition()
@@ -96,69 +138,74 @@ d3.dsv('\\', './data/US_final.csv').then(function(data) {
             reset_all_circles();
             remove_path();
         });
-        
-focus.append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
 
-focus.append("g")
-      .attr("class", "axis axis--y")
-      .call(yAxis);
-      
-// focus.append("text")
-//       .attr("transform", "rotate(-90)")
-//       .attr("y", 0 - margin.left)
-//       .attr("x",0 - (height / 2))
-//       .attr("dy", "1em")
-//       .style("text-anchor", "middle")
-//       .style("fill", "white")
-//       .text("Views");  
-      
-// svg.append("text")             
-//       .attr("transform",
-//             "translate(" + ((width + margin.right + margin.left)/2) + " ," + 
-//                            (height + margin.top + margin.bottom) + ")")
-//       .style("text-anchor", "middle")
-//       .text("Trending Date");
-      
-// append scatter plot to brush chart area      
- var dots = context.append("g");
-     dots.attr("clip-path", "url(#clip)");
-     dots.selectAll("dot")
-        .data(data)
-        .enter().append("circle")
-        .attr('class', 'dotContext')
-        .attr("r",1)
-        .style("opacity", .5)
-        .style('fill', color)
-        .attr("cx", function(d) { return x2(d.trending_date); })
-        .attr("cy", function(d) { return y2(d.views); })
-        
-context.append("g")
-      .attr("class", "axis axis--y")
-      .attr("transform", "translate("+ width2+"," + 0 + ")")
-      .call(yAxis2);
+    focus.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
 
-context.append("g")
-      .attr("class", "brush")
-      .call(brush)
-      .call(brush.move, y2.range());
+    focus.append("g")
+        .attr("class", "axis axis--y")
+        .call(yAxis);
+
+    // focus.append("text")
+    //       .attr("transform", "rotate(-90)")
+    //       .attr("y", 0 - margin.left)
+    //       .attr("x",0 - (height / 2))
+    //       .attr("dy", "1em")
+    //       .style("text-anchor", "middle")
+    //       .style("fill", "white")
+    //       .text("Views");  
+
+    // svg.append("text")             
+    //       .attr("transform",
+    //             "translate(" + ((width + margin.right + margin.left)/2) + " ," + 
+    //                            (height + margin.top + margin.bottom) + ")")
+    //       .style("text-anchor", "middle")
+    //       .text("Trending Date");
+
+    // append scatter plot to brush chart area      
+    // var dots = context.append("g");
+    //     dots.attr("clip-path", "url(#clip)");
+    //     dots.selectAll("dot")
+    //        .data(data)
+    //        .enter().append("circle")
+    //        .attr('class', 'dotContext')
+    //        .attr("r",1)
+    //        .style("opacity", .5)
+    //        .style('fill', color)
+    //        .attr("cx", function(d) { return x2(d.trending_date); })
+    //        .attr("cy", function(d) { return y2(d.views); })
+
+    // context.append("g")
+    //       .attr("class", "axis axis--y")
+    //       .attr("transform", "translate("+ width2+"," + 0 + ")")
+    //       .call(yAxis2);
+
+    context.append("g")
+        .attr("class", "brush")
+        .call(brush)
+        .call(brush.move, y2.range());
     brush_flag = 1;
 });
 
 //create brush function redraw scatterplot with selection
 function brushed() {
-  var selection = d3.event.selection;
-  var newDomain =[selection[1],selection[0]].map(y2.invert, y)
-  y.domain(newDomain);
-  // console.log(newDomain);
-  if(brush_flag){
-    focus.selectAll(".dotFocus")
-        .attr("cx", function(d) { return x(d.trending_date); })
-        .attr("cy", function(d) { return y(d.views); });
-    focus.select(".axis--y").call(yAxis);
-  }
+
+    // console.log(newDomain);
+    if (brush_flag) {
+        var selection = d3.event.selection;
+        var newDomain = [selection[1], selection[0]].map(y2.invert, y)
+        y.domain(newDomain);
+        focus.selectAll(".dotFocus")
+            // .attr("cx", function(d) {
+            //     return x(d.trending_date);
+            // })
+            .attr("cy", function(d) {
+                return y(d.views);
+            });
+        focus.select(".axis--y").call(yAxis);
+    }
 }
 
 
@@ -224,6 +271,67 @@ function reset_all_circles() {
         .attr("r", radius);
 }
 
+function update_svg(value) {
+    if (value != curr_y) {
+        console.log(value)
+        var yExtent = d3.extent(videos, function(d) {
+            return +d[value];
+        });
+        console.log(yExtent);
+        var yOffset = 0.01 * (yExtent[1] - yExtent[0]);
+        yExtent = [yExtent[0] - yOffset, yExtent[1] + yOffset];
+
+
+        y.domain(yExtent);
+        y2.domain(y.domain());
+
+        svg.selectAll(".dotFocus")
+            .transition()
+            .duration(600)
+            .attr("cx", function(d) {
+                return x(d.trending_date);
+            })
+            .attr("cy", function(d) {
+                return y(d[value]);
+            });
+
+        brush_flag = 0
+
+        function brushed() {
+            var selection = d3.event.selection;
+            var newDomain = [selection[1], selection[0]].map(y2.invert, y)
+            y.domain(newDomain);
+            // console.log(newDomain);
+            if (brush_flag) {
+                focus.selectAll(".dotFocus")
+                    .attr("cx", function(d) {
+                        return x(d.trending_date);
+                    })
+                    .attr("cy", function(d) {
+                        return y(d[value]);
+                    });
+                focus.select(".axis--y").call(yAxis);
+            }
+        }
+
+        brush = d3.brushY()
+            .extent([
+                [0, 0],
+                [width2, height]
+            ])
+            .on("brush", brushed);
+        context.select(".brush")
+            .call(brush)
+            .call(brush.move, y2.range());
+        brush_flag = 1;
+        focus.select(".axis--y")
+            .call(yAxis);
+        curr_y = value;
+    }
+}
+/*helper JS*/
+
+// FOR DATEPICKER
 $('input[name="daterange"]').daterangepicker({
     opens: 'left',
     drops: "up"
@@ -231,7 +339,20 @@ $('input[name="daterange"]').daterangepicker({
     //set up date range in breakdown
     bd_start = start;
     bd_end = end;
-    document.getElementById("bd-date").innerHTML = start.format('YYYY-MM-DD') + ' - ' + end.format('YYYY-MM-DD') ;
-    
+    document.getElementById("bd-date").innerHTML = start.format('YYYY-MM-DD') + ' - ' + end.format('YYYY-MM-DD');
+
     console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
 });
+
+// FOR TAGS SELECTION
+$('.ui-choose').ui_choose();
+var uc_02 = $('#uc_02').data('ui-choose');
+uc_02.click = function(value, item) {
+    console.log('click', value);
+
+};
+uc_02.change = function(value, item) {
+    bd_sortfunc();
+    console.log('change', value);
+    update_svg(value);
+};
